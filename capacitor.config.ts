1bbdec85ef4b3735760fa3b059f51d95eff0db66
Chingun Ariunbat <chingun26@yahoo.de>
2023-01-20 18:22:46 +0100

    init commit with capacitor and angular material

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'de.ariunbat.moonlight',
+  appName: 'Moonlight',
+  webDir: 'dist/moonlight',
+  bundledWebRuntime: false
+};
+
+export default config;
